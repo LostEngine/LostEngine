c98787132cc19ec67ba08e734ca5bc1946798fbc
misieur <misieur.off@gmail.com>
2025-12-31 14:26:41 +0100

    1.21.11

--- src/main/java/dev/lost/engine/bootstrap/components/UseCooldownProperty.java
+++ src/main/java/dev/lost/engine/bootstrap/components/UseCooldownProperty.java
@@ -5,7 +5,7 @@ import dev.lost.engine.bootstrap.components.annotations.Property;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.component.UseCooldown;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
@@ -24,9 +24,9 @@ public class UseCooldownProperty implements ComponentProperty {
 
     @Override
     public void applyComponent(@NotNull BootstrapContext context, @NotNull ConfigurationSection itemSection, @NotNull String itemID, @NotNull Map<DataComponentType<?>, Object> components) {
-        Optional<ResourceLocation> group = groupString == null ?
-                Optional.of(ResourceLocation.fromNamespaceAndPath("lost_engine", itemSection.getName())) :
-                Optional.of(ResourceLocation.parse(groupString));
+        Optional<Identifier> group = groupString == null ?
+                Optional.of(Identifier.fromNamespaceAndPath("lost_engine", itemSection.getName())) :
+                Optional.of(Identifier.parse(groupString));
 
         components.put(DataComponents.USE_COOLDOWN, new UseCooldown(cooldownSeconds, group));
     }
