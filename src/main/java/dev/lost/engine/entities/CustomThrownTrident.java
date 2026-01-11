2d6454c2f2869d995692056cd2925f8208aa290b
misieur <misieur.off@gmail.com>
2026-01-11 19:20:56 +0100

    Delay removal of custom tridents from the map 1 second asynchronously

--- src/main/java/dev/lost/engine/entities/CustomThrownTrident.java
+++ src/main/java/dev/lost/engine/entities/CustomThrownTrident.java
@@ -1,14 +1,17 @@
 package dev.lost.engine.entities;
 
+import dev.lost.engine.LostEngine;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.arrow.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 public class CustomThrownTrident extends ThrownTrident {
 
@@ -26,7 +29,12 @@ public class CustomThrownTrident extends ThrownTrident {
 
     public void onRemoval(@NotNull Entity.RemovalReason reason) {
         super.onRemoval(reason);
-        CUSTOM_TRIDENTS.remove(this.getId());
+        Bukkit.getAsyncScheduler().runDelayed(
+                LostEngine.getInstance(),
+                scheduledTask -> CUSTOM_TRIDENTS.remove(this.getId()),
+                1,
+                TimeUnit.SECONDS
+        ); // We remove the trident from the map asynchronously 1 second after to make sure all metadata packets have been sent
     }
 
     public void setPickupItemStack(@NotNull ItemStack pickupItemStack) {
