ee97b5676202cf916ed3dac2f43140122a8d0305
misieur <misieur.off@gmail.com>
2026-01-18 10:53:44 +0100

    Improve boostrap

--- src/main/java/dev/lost/engine/bootstrap/LostEngineBootstrap.java
+++ src/main/java/dev/lost/engine/bootstrap/LostEngineBootstrap.java
@@ -3,16 +3,19 @@ package dev.lost.engine.bootstrap;
 import dev.lost.engine.LostEngine;
 import dev.lost.engine.annotations.CanBreakOnUpdates;
 import dev.lost.engine.assetsgenerators.DataPackGenerator;
+import dev.lost.engine.utils.TimeUtils;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
 import net.minecraft.SharedConstants;
+import org.bukkit.craftbukkit.util.CraftMagicNumbers;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 
 import static dev.lost.engine.bootstrap.ResourceInjector.injectResources;
@@ -25,15 +28,19 @@ public class LostEngineBootstrap implements PluginBootstrap {
         try {
             String versionId = SharedConstants.getCurrentVersion().id();
             if (!versionId.equals("1.21.11")) {
-                context.getLogger().error("LostEngine only supports Minecraft/Paper 1.21.11, detected version: {}", versionId);
+                context.getLogger().error("This version of LostEngine only supports Minecraft/Paper 1.21.11, detected version: {}", versionId);
                 stopServer(context);
             }
-            //noinspection unused -- Force class loading to fix a random bug
-            final net.minecraft.world.level.block.Block air = net.minecraft.world.level.block.Blocks.AIR;
+            context.getLogger().info("Loading Minecraft classes...");
+            long startTime = System.nanoTime();
+            Objects.requireNonNull(net.minecraft.world.level.block.Blocks.AIR);
+            Objects.requireNonNull(net.minecraft.world.item.Items.AIR);
+            Objects.requireNonNull(CraftMagicNumbers.INSTANCE);
+            long elapsedNanos = System.nanoTime() - startTime;
 
             // This is a personalized version of Nancyj-Improved
             context.getLogger().info("""
-                    
+                    Finished loading Minecraft classes! ({})
                     dP                                    88888888b
                     88                            88       88                            oo
                     88        .d8888b. .d8888b. d8888P    a88aaaa    88d888b. .d8888b. d888   88d888b. .d8888b.
@@ -41,12 +48,15 @@ public class LostEngineBootstrap implements PluginBootstrap {
                     88        88.  .88       88   88       88        88    88 88.  .88   88   88    88 88.  ...
                     88888888P `88888P' `88888P'   `8b.    88888888P  dP    dP `8888P88 d8888P dP    dP `88888P'
                                                                                    .88
-                                                                               d8888P""");
+                                                                               d8888P""", TimeUtils.formatNanos(elapsedNanos));
             context.getLogger().info("Start injecting custom items...");
+            startTime = System.nanoTime();
             DataPackGenerator dataPackGenerator = new DataPackGenerator();
             injectResources(context, dataPackGenerator);
-            context.getLogger().info("Finished injecting custom items!");
+            elapsedNanos = System.nanoTime() - startTime;
+            context.getLogger().info("\u001b[1A\u001b[2KFinished injecting custom items! ({})", TimeUtils.formatNanos(elapsedNanos));
             context.getLogger().info("Start building the data pack...");
+            startTime = System.nanoTime();
 
             // Load server properties to get the world name
             Properties props = new Properties();
@@ -59,7 +69,8 @@ public class LostEngineBootstrap implements PluginBootstrap {
                 levelName = "world";
             }
             dataPackGenerator.build(new File(levelName + File.separator + "datapacks" + File.separator + "lost_engine_generated"));
-            context.getLogger().info("Finished building the data pack!");
+            elapsedNanos = System.nanoTime() - startTime;
+            context.getLogger().info("\u001b[1A\u001b[2KFinished building the data pack! ({})", TimeUtils.formatNanos(elapsedNanos));
         } catch (Exception e) {
             context.getLogger().error("Failed to inject custom resources are you using Minecraft/Paper 1.21.11?", e);
             stopServer(context);
---
+++ src/main/java/dev/lost/engine/utils/TimeUtils.java
@@ -0,0 +1,12 @@
+package dev.lost.engine.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TimeUtils {
+    public static @NotNull String formatNanos(long nanos) {
+        if (nanos < 1_000) return nanos + " ns";
+        if (nanos < 1_000_000) return String.format("%.2f μs", nanos / 1_000.0);
+        if (nanos < 1_000_000_000L) return String.format("%.2f ms", nanos / 1_000_000.0);
+        return String.format("%.2f s", nanos / 1_000_000_000.0);
+    }
+}
