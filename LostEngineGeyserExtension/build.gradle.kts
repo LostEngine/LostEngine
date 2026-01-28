3693daf6ec86fe5c9d3334177e52043a5c5f0dcb
misieur <misieur.off@gmail.com>
2026-01-28 09:56:31 +0100

    Use https://repo.misieur.me/repository for geyser api v2

--- LostEngineGeyserExtension/build.gradle.kts
+++ LostEngineGeyserExtension/build.gradle.kts
@@ -7,18 +7,17 @@ version = "1.0-SNAPSHOT"
 val id = "lostenginegeyserextension"
 val extensionName = "LostEngineGeyserExtension"
 val author = "Misieur"
-val geyserApiVersion = "2.9.0"
+val geyserApiVersion = "2.9.2"
 
 repositories {
     mavenCentral()
     mavenLocal()
-    maven("https://jitpack.io")
     maven("https://repo.opencollab.dev/main/")
+    maven("https://repo.misieur.me/repository")
 }
 
 dependencies {
     compileOnly("org.geysermc.geyser:api:2.9.2-SNAPSHOT")
-    //compileOnly("org.geysermc.geyser:api:$geyserApiVersion-SNAPSHOT")
     // Using the same versions as Geyser for compatibility
     compileOnly("it.unimi.dsi:fastutil:8.5.15")
     compileOnly("com.google.code.gson:gson:2.3.1")
