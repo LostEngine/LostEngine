4fc707b4da43fd6abd8aefba643a59f018dfd744
misieur <misieur.off@gmail.com>
2026-03-21 22:23:11 +0100

    Update sword.lua

--- src/main/resources/resources/default/scripts/sword.lua
+++ src/main/resources/resources/default/scripts/sword.lua
@@ -2,7 +2,7 @@
 ---@param blockHit BlockHit
 function onClick(player, blockHit)
     if blockHit ~= nil then
-        local x, y, z = blockHit.getPosition()
+        local x, y, z = blockHit.getLocation()
         player.sendMessage("You clicked at " .. x .. "," .. y .. "," .. z)
         player.getWorld().spawnEntity("minecraft:lightning_bolt", x, y, z)
     end
